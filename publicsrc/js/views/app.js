404eb46f4529d4113ce812d8684271bd9b8ce681
Josh Zeltman <joshua.zeltman@gmail.com>
2019-01-31 15:40:27 -0300

    Firebase based login supported, persistent user status, styled user profile image

--- publicsrc/js/views/app.js
+++ publicsrc/js/views/app.js
@@ -1,7 +1,28 @@
 export default class AppView {
     constructor(el){
         this.$el        = document.querySelector('body');
+        this.$header    = this.$el.querySelector('header');
         this.$user      = this.$el.querySelector('#user');
         this.$main      = this.$el.querySelector('main');
+
+        this.$user.addEventListener('click',this.userProfileClickHandler.bind(this));
+
+        window.events.sub('auth:change', this.updateUserProfile.bind(this)); 
+    }
+
+    updateUserProfile(){
+
+        if (window.user !== null){
+            try {
+            console.log('foooooo',window.user,window.user.data.photoURL);
+                this.$user.innerHTML = `<img src=${window.user.data.photoURL} class="user-profile-img" />`;
+                this.$header.classList.remove('logged-out');
+                this.$header.classList.add('logged-in');
+            } catch(e) { console.error(e); }
+        }
+    }
+
+    userProfileClickHandler(){
+        console.log('userProfileClickHandler',window.user);
     }
 }
